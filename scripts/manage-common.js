fc13df708054c74757bc368cf5a7686751fda5c4
AfterWin <CJ_Zheng1023@hotmail.com>
2017-07-27 11:59:33 +0800

    管理员页面user，topic

--- scripts/manage-common.js
+++ scripts/manage-common.js
@@ -25,5 +25,22 @@ $(function(){
 
     })
 
+    //初始化icheck插件
+    $('input:checkbox').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_minimal-blue',
+        increaseArea: '20%' // optional
+    });
+    //全选
+    $(".group-checkable").on("ifChecked", function(){
+        var set = $(this).attr("data-set");
+        $(set).iCheck("check");
+    })
+    //反选
+    $(".group-checkable").on("ifUnchecked", function(){
+        var set = $(this).attr("data-set");
+        $(set).iCheck("uncheck");
+    })
+
 
 })
